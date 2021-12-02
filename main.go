df691a6797a47a9719eb6530248727368b63a298
flieslord <95360880+flieslord@users.noreply.github.com>
2021-12-02 21:53:55 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
-
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 func main() {
-	fmt.Println("Hello World")
+	r := gin.New()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+	r.Run(":8080")
 }
