11e4dde2d42381ad1f1f2c26e4f1be3a59a8cccc
flieslord <95360880+flieslord@users.noreply.github.com>
2021-12-02 21:46:24 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"net/http"
-	"fmt"
-)
+import "fmt"
+
 func main() {
-	r := gin.New()
-	fmt.Println("服务器开始运行在8080端口")
-	r.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "hello world",
-		})
-	})
-	r.Run(":8080")
+	fmt.Println("Hello World")
 }
